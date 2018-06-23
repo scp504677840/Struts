208d6605e5287f40a1027afe670deac0fc5cee01
y1笑而过 <15335747148@163.com>
2018-06-23 16:41:58 +0800

    空工程
    空工程

---
+++ 空工程/src/main/TestAction.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * https://blog.csdn.net/hfut_wowo/article/details/62887855
+ */
+public class TestAction {
+    public String execute() {
+        return "success";
+    }
+}
